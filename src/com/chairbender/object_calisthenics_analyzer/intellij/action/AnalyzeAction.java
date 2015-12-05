1bec118b4c1bfd1b29ffb09c32f560bfe1b43b4e
chairbender <kwhipke1@gmail.com>
2015-12-04 22:03:10 -0800

    modified it so it doesn't block the UI thread when running an analysis

--- src/com/chairbender/object_calisthenics_analyzer/intellij/action/AnalyzeAction.java
+++ src/com/chairbender/object_calisthenics_analyzer/intellij/action/AnalyzeAction.java
@@ -3,9 +3,11 @@ package com.chairbender.object_calisthenics_analyzer.intellij.action;
 import com.chairbender.object_calisthenics_analyzer.ObjectCalisthenicsAnalyzer;
 import com.chairbender.object_calisthenics_analyzer.intellij.ui.ReportComponent;
 import com.chairbender.object_calisthenics_analyzer.violation.ViolationMonitor;
+import com.github.javaparser.ParseException;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.wm.ToolWindow;
@@ -28,29 +30,52 @@ public class AnalyzeAction extends AnAction {
     private static ToolWindow reportToolWindow;
 
     public void actionPerformed(AnActionEvent anActionEvent) {
-        Project project = (Project) anActionEvent.getData(PlatformDataKeys.PROJECT);
+        final Project project = (Project) anActionEvent.getData(PlatformDataKeys.PROJECT);
         //Run a report, spit the text out into a tool window
-        try {
-            ViolationMonitor analysis = ObjectCalisthenicsAnalyzer.analyze(new File(project.getBasePath()), "UTF-8");
-            //create that tool window
-            ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(project);
-            if (reportToolWindow == null) {
-                reportToolWindow = toolWindowManager.registerToolWindow(Constants.REPORT_WINDOW_ID, true, ToolWindowAnchor.BOTTOM);
-                reportToolWindow.setTitle("Calisthenics");
-            }
 
-            ContentManager reportWindowContentManager = reportToolWindow.getContentManager();
-            reportWindowContentManager.removeAllContents(true);
-            ReportComponent reportComponent = new ReportComponent(analysis,project);
-            Content reportContent = reportWindowContentManager.getFactory().createContent(reportComponent,"Report",false);
-            reportWindowContentManager.addContent(reportContent);
-            reportToolWindow.activate(null);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (com.github.javaparser.ParseException e) {
-            e.printStackTrace();
-        } catch (BadLocationException e) {
-            e.printStackTrace();
-        }
+        ApplicationManager.getApplication().executeOnPooledThread(new Runnable() {
+            public void run() {
+                ApplicationManager.getApplication().runReadAction(new Runnable() {
+                      public void run() {
+                          // do whatever you need to do
+                          ViolationMonitor analysis = null;
+                          try {
+                              analysis = ObjectCalisthenicsAnalyzer.analyze(new File(project.getBasePath()), "UTF-8");
+                          } catch (IOException e) {
+                              e.printStackTrace();
+                          } catch (ParseException e) {
+                              e.printStackTrace();
+                          }
+                          final ViolationMonitor finalAnalysis = analysis;
+                          ApplicationManager.getApplication().invokeLater(new Runnable() {
+                              @Override
+                              public void run() {
+                                  //create that tool window
+                                  ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(project);
+                                  if (reportToolWindow == null) {
+                                      reportToolWindow = toolWindowManager.registerToolWindow(Constants.REPORT_WINDOW_ID, true, ToolWindowAnchor.BOTTOM);
+                                      reportToolWindow.setTitle("Calisthenics");
+                                  }
+
+                                  ContentManager reportWindowContentManager = reportToolWindow.getContentManager();
+                                  reportWindowContentManager.removeAllContents(true);
+                                  ReportComponent reportComponent = null;
+                                  try {
+                                      reportComponent = new ReportComponent(finalAnalysis, project);
+                                  } catch (BadLocationException e) {
+                                      e.printStackTrace();
+                                  }
+                                  Content reportContent = reportWindowContentManager.getFactory().createContent(reportComponent, "Report", false);
+                                  reportWindowContentManager.addContent(reportContent);
+                                  reportToolWindow.activate(null);
+                              }
+                          });
+                      }
+                  }
+
+                );
+            }
+        });
+
     }
 }
